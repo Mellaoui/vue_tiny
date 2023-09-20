35747a633fd9119756db866da6a31df6a94e065c
Mellaoui Mohammed elghazali <Modijavelin@gmail.com>
2023-09-20 16:01:21 +0100

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<template>
-  <!--
-    This example requires updating your template:
 
-    ```
-    <html class="h-full bg-white">
-    <body class="h-full">
-    ```
-  -->
-  <div class="dark:bg-gray-700">
+<template>
+ 
+  <div class="dark:bg-gray-900">
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog as="div" class="relative z-50 lg:hidden" @close="sidebarOpen = false">
         <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
@@ -46,7 +26,7 @@
               </TransitionChild>
               <!-- Sidebar component, swap this element with another sidebar if you like -->
               <div
-                class="flex flex-col px-6 pb-4 overflow-y-auto bg-slate-50 dark:bg-gray-900 grow gap-y-5 ring-1 ring-white/10">
+                class="flex flex-col px-6 pb-4 overflow-y-auto bg-slate-50 dark:bg-gray-700 grow gap-y-5 ring-1 ring-white/10">
                 <div class="flex items-center h-16 shrink-0">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-auto h-8" viewBox="0 0 128 128">
                     <path
@@ -102,7 +82,7 @@
     <!-- Static sidebar for desktop -->
     <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex flex-col px-6 pb-4 overflow-y-auto bg-slate-50 dark:bg-gray-900 grow gap-y-5">
+      <div class="flex flex-col px-6 pb-4 overflow-y-auto bg-slate-50 dark:bg-gray-700 grow gap-y-5">
         <div class="flex items-center h-16 shrink-0">
           <svg xmlns="http://www.w3.org/2000/svg" class="w-auto h-8" viewBox="0 0 128 128">
             <path
@@ -154,7 +134,7 @@
     <div class="lg:pl-72">
       <div
         class="sticky top-0 z-40 flex items-center h-16 px-4 transition-all shrink-0 gap-x-4 sm:gap-x-6 sm:px-6 lg:px-8"
-        :class="{ 'bg-white dark:bg-slate-900 border-gray-200 shadow-sm border-b transition-all': isScrolled }">
+        :class="{ 'bg-white dark:bg-slate-700 border-gray-200 shadow-sm border-b transition-all': isScrolled }">
         <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
           <span class="sr-only">Open sidebar</span>
           <Bars3Icon class="w-6 h-6" aria-hidden="true" />
@@ -169,8 +149,8 @@
             <MagnifyingGlassIcon class="absolute inset-y-0 left-0 w-5 h-full text-gray-400 pointer-events-none"
               aria-hidden="true" />
             <input id="search-field"
-              class="block w-full h-full py-0 pl-8 pr-0 text-gray-900 bg-transparent border-0 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-              :class="{ 'bg-white dark:bg-slate-900 dark:text-white': isScrolled }" placeholder="Search..." type="search"
+              class="block w-full h-full py-0 pl-8 pr-0 text-gray-900 bg-transparent border-0 placeholder:text-gray-400 sm:text-sm"
+              :class="{ 'bg-white dark:bg-slate-700 dark:text-white': isScrolled }" placeholder="Search..." type="search"
               name="search" />
           </form>
           <div class="flex items-center gap-x-4 lg:gap-x-6">
@@ -196,7 +176,7 @@
                   <div
                     class="overflow-hidden bg-gray-200 divide-y divide-gray-200 rounded-lg shadow dark:bg-slate-400 sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
                     <div v-for="(action, actionIdx) in actions" :key="action.title"
-                      :class="[actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', actionIdx === 1 ? 'sm:rounded-tr-lg' : '', actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '', actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'group relative dark:bg-slate-900 bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-500']">
+                      :class="[actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', actionIdx === 1 ? 'sm:rounded-tr-lg' : '', actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '', actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'group relative dark:bg-slate-700 bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-500']">
                       <div>
                         <span
                           :class="[action.iconBackground, action.iconForeground, 'inline-flex rounded-lg p-3 ring-4 ring-white']">
@@ -236,17 +216,17 @@
                 <PopoverPanel>
                   <div
                     class="absolute z-10 flex flex-col w-screen h-full max-w-sm px-4 mt-3 transform -translate-x-12 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 max-h-44 left-3/4 sm:px-0 lg:max-w-sm">
-                    <div class="p-4 bg-gray-50">
+                    <div class="p-4 bg-gray-50 dark:bg-slate-700">
                       <a href="##"
                         class="flow-root px-2 py-2 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
                         <div class="flex flex-row justify-between">
                           <span class="flex items-center">
-                            <span class="text-sm font-medium text-gray-900">
+                            <span class="text-sm font-medium text-gray-900 dark:text-gray-300">
                               Notifications
                             </span>
                           </span>
-                          <span class="block text-sm text-gray-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                          <span class="block text-sm text-gray-500 dark:text-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"  viewBox="0 0 24 24" stroke-width="1.5"
                               stroke="currentColor" class="w-6 h-6">
                               <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
@@ -256,7 +236,7 @@
                         </div>
                       </a>
                     </div>
-                    <div class="relative grid gap-8 bg-white p-7 lg:grid-cols-1">
+                    <div class="relative grid gap-8 bg-white dark:bg-slate-700 p-7 lg:grid-cols-1">
                       <a v-for="item in notifications" :key="item.name" :href="item.href"
                         class="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
                         <div class="flex items-center justify-center w-10 h-10 text-white shrink-0 sm:h-12 sm:w-12">
@@ -264,16 +244,16 @@
                             alt="user--v1" />
                         </div>
                         <div class="ml-4">
-                          <p class="text-sm font-medium text-gray-900">
+                          <p class="text-sm font-medium text-gray-900 dark:text-gray-100">
                             {{ item.name }}
                           </p>
-                          <p class="text-sm text-gray-500">
+                          <p class="text-sm text-gray-500 dark:text-gray-200">
                             {{ item.description }}
                           </p>
                         </div>
                       </a>
                     </div>
-                    <div class="p-4 bg-gray-50">
+                    <div class="p-4 dark:bg-slate-700 bg-gray-50">
                       <a href="##"
                         class="flow-root px-2 py-2 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
                         <span class="flex items-center justify-center">
@@ -355,18 +335,18 @@
       </TransitionRoot>
     </div>
     <footer
-      :class="[isSticky ? 'sticky bottom-0 bg-white dark:bg-slate-900 transition-all duration-75' : 'transition-all duration-75']">
+      :class="[isSticky ? 'sticky bottom-0 bg-white dark:bg-slate-700 transition-all duration-75' : 'transition-all duration-75']">
       >
       <div class="px-6 py-4 mx-auto max-w-7xl md:flex md:items-center md:justify-between lg:px-8">
         <div class="flex justify-center space-x-6 md:order-2">
           <a v-for="item in navigation_footer " :key="item.name" :href="item.href"
             class="text-gray-400 hover:text-gray-500">
             <span class="sr-only">{{ item.name }}</span>
-            <component :is="item.icon" class="w-6 h-6" aria-hidden="true" />
+          <a :href="item.href">{{ item.name }}</a>
           </a>
         </div>
         <div class="mt-8 md:order-1 md:mt-0">
-          <p class="text-xs leading-5 text-center text-gray-500">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+          <p class="text-xs leading-5 text-center text-gray-300">&copy; 2020 Your Company, Inc. All rights reserved.</p>
         </div>
       </div>
     </footer>
@@ -553,70 +533,22 @@ const navigation_footer = [
   {
     name: 'Facebook',
     href: '#',
-    icon: defineComponent({
-      render: () =>
-        h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-          h('path', {
-            'fill-rule': 'evenodd',
-            d: 'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z',
-            'clip-rule': 'evenodd',
-          }),
-        ]),
-    }),
   },
   {
     name: 'Instagram',
     href: '#',
-    icon: defineComponent({
-      render: () =>
-        h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-          h('path', {
-            'fill-rule': 'evenodd',
-            d: 'M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z',
-            'clip-rule': 'evenodd',
-          }),
-        ]),
-    }),
   },
   {
     name: 'Twitter',
     href: '#',
-    icon: defineComponent({
-      render: () =>
-        h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-          h('path', {
-            d: 'M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84',
-          }),
-        ]),
-    }),
   },
   {
     name: 'GitHub',
     href: '#',
-    icon: defineComponent({
-      render: () =>
-        h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-          h('path', {
-            'fill-rule': 'evenodd',
-            d: 'M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z',
-            'clip-rule': 'evenodd',
-          }),
-        ]),
-    }),
   },
   {
     name: 'YouTube',
     href: '#',
-    icon: defineComponent({
-      render: () =>
-        h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-          h('path', {
-            'fill-rule': 'evenodd',
-            d: 'M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z',
-            'clip-rule': 'evenodd',
-          }),
-        ]),
-    }),
   },
 ]
 
