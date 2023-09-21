cf57faabfff8ceba9b40d6a1cff81ceb0d2fb12d
Mellaoui Mohammed elghazali <Modijavelin@gmail.com>
2023-09-21 18:52:20 +0100

    new commit

--- src/App.vue
+++ src/App.vue
@@ -590,8 +590,8 @@
                                 <li>
                                   <ul role="list" class="-mx-2 space-y-1">
                                     <li v-for="item in navigation" :key="item.name">
-                                      <a :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                                        <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+                                      <a :href="item.href" :class="[item.current ? 'bg-gradient-to-r from-orange-600 to-orange-400 text-white' : 'text-gray-600 hover:text-white hover:bg-gradient-to-r from-orange-600 to-orange-400', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                                        <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-gray-600 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
                                         {{ item.name }}
                                       </a>
                                     </li>
